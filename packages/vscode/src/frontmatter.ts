129bb7560f64c941e6a27a47afc8ebcfdebc46f2
pelikhan <jhalleux@microsoft.com>
2023-05-22 16:44:03 -0700

    drop remark

--- packages/vscode/src/frontmatter.ts
+++ packages/vscode/src/frontmatter.ts
@@ -1,90 +1,74 @@
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkStringify from "remark-stringify";
 import { parse, stringify } from "yaml";
-import { Root, YAML } from "mdast";
-import { toMarkdown } from "mdast-util-to-markdown";
 import { createChatCompletion } from "./openai";
 
 export interface SeoOptions {
   model?: string;
   openApiKey: string;
+  temperature?: number;
+}
+
+function tryParseYaml(source: string | undefined) {
+  try {
+    return source ? parse(source) : {};
+  } catch (e) {
+    console.log(e);
+    return {};
+  }
 }
 
 export async function generateFrontMatter(
   content: string,
   options: SeoOptions
 ) {
-  const { model = "gpt-3.5-turbo", openApiKey } = options;
-  const temperature = 0.0;
-
-  // read file
-  let modified = false;
+  const { model = "gpt-3.5-turbo", openApiKey, temperature = 0.0 } = options;
 
   // parse as markdown
-  const out = await unified()
-    .use(remarkParse)
-    .use(remarkStringify)
-    .use(remarkFrontmatter, ["yaml"])
-    .use(() => async (root: Root) => {
-      const node = root.children[0]?.type === "yaml";
-      if (node) {
-        const node = root.children[0] as YAML;
-        const frontmatter = parse(node.value);
+  const m = /^(---\n(?<frontmatter>.*)---\n)?(?<markdown>.*)$/s.exec(content);
+  if (!m) return {};
+  const { frontmatter, markdown } = m.groups || {};
+  if (!markdown) return {};
 
-        const rootNoFrontMatter = JSON.parse(JSON.stringify(root));
-        rootNoFrontMatter.children.pop(); // ignore previous frontmatter
-
-        const promptMarkdown = toMarkdown(rootNoFrontMatter, {
-          handlers: {
-            yaml: (node: YAML) => node.value,
-          },
-        });
-
-        console.log(`generating frontmatter...`);
-        const completion = await createChatCompletion(
-          {
-            model,
-            messages: [
-              {
-                role: "system",
-                content: `You are a front matter generator for mardown. 
+  console.log(`generating frontmatter...`);
+  const completion = await createChatCompletion(
+    {
+      model,
+      messages: [
+        {
+          role: "system",
+          content: `You are a front matter generator for mardown. 
 - You generate the title, description, keywords for the markdown given by the user
 - use yaml format 
 - do not generate the \`---\` fences
 - optimize for SEO
 `,
-              },
-              {
-                role: "user",
-                content: promptMarkdown,
-              },
-            ],
-            temperature,
-          },
-          openApiKey
-        );
-        const fm = completion.data?.choices?.[0]?.message?.content;
-        const ryaml = parse(fm!.replace(/^---/, "").replace(/---$/, ""), {
-          prettyErrors: true,
-        }) as {
-          title: string;
-          description: string;
-          keywords: string;
-        };
-        const { title, description, keywords } = ryaml;
+        },
+        {
+          role: "user",
+          content: markdown,
+        },
+      ],
+      temperature,
+    },
+    openApiKey
+  );
+  const fm = completion.data?.choices?.[0]?.message?.content;
+  const ryaml = parse(fm!.replace(/^---/, "").replace(/---$/, ""), {
+    prettyErrors: true,
+  }) as {
+    title: string;
+    description: string;
+    keywords: string;
+  };
+  const { title, description, keywords } = ryaml;
 
-        const newFrontMatter = { ...frontmatter, title, description, keywords };
-        if (JSON.stringify(frontmatter) !== JSON.stringify(newFrontMatter)) {
-          node.value = stringify(newFrontMatter);
-          modified = true;
-        }
-      }
-    })
-    .process(content);
+  const newFrontMatter = {
+    ...tryParseYaml(frontmatter),
+    title,
+    description,
+    keywords,
+  };
 
-  const output = String(out);
-  console.log(output);
-  return { modified, output };
+  const output = `---\n${stringify(newFrontMatter)}---\n${markdown}`;
+
+  return { output };
 }
